2ed3589f0ad35d5e81a189ad60481e6c11794029
joao <joaovitorsantossouza937@gmail.com>
2026-09-04 18:28:59 -0300

    feat: creates the actions

--- src/redux/user/slice.jsx
+++ src/redux/user/slice.jsx
@@ -4,14 +4,25 @@ const initialState = {
     user: null
 }
 
-
 export const userSlice = createSlice({
     name: "user",
     initialState,
-    reducers:{
+    reducers: {
+        createUser: (state, action) => {
+            console.log(action.payload)
 
+            return {
+                ...state,
+                user: {
+                    name: action.payload.name,
+                    email: action.payload.email,
+                    address: null
+                }
+            }
+            
+        }
     }
 })
 
-
+export const { createUser } = userSlice.actions;
 export default userSlice.reducer;
